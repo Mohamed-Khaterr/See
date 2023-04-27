665e5dc13226e8a180ad637d44259ddaeb59fd4a
Mohamed Khater <mohamed.15751@gmail.com>
2023-04-26 17:21:20 -0700

    Add NotificationCenter to notify any observer that user is logged in or logged out

--- See/Service/User.swift
+++ See/Service/User.swift
@@ -15,18 +15,23 @@ struct User {
     private let tokenKey = "requestToken"
     private let sessionIDKey = "sessionId"
     
+    private let notificationCenter = NotificationCenter.default
+    public static let loginNotificationKey = "User.loggedIn"
+    public static let logoutNotificationKey = "User.loggedOut"
+    
     private init() {}
     
     func setToken(_ token: String){
         userDefaults.set(token, forKey: tokenKey)
     }
     
-    func getToken() -> String?{
-        return userDefaults.string(forKey: tokenKey)
-    }
-    
     func setSessionID(_ sessionID: String){
         userDefaults.set(sessionID, forKey: sessionIDKey)
+        sendNotifictaionForLogin()
+    }
+    
+    func getToken() -> String?{
+        return userDefaults.string(forKey: tokenKey)
     }
     
     func getSessionID() -> String?{
@@ -40,5 +45,14 @@ struct User {
     func logout(){
         userDefaults.removeObject(forKey: tokenKey)
         userDefaults.removeObject(forKey: sessionIDKey)
+        sendNotificationForLogout()
+    }
+    
+    private func sendNotifictaionForLogin() {
+        notificationCenter.post(name: Notification.Name(User.loginNotificationKey), object: nil)
+    }
+    
+    private func sendNotificationForLogout() {
+        notificationCenter.post(name: Notification.Name(User.logoutNotificationKey), object: nil)
     }
 }
